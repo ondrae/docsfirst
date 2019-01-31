92e173c814dc1f86499f27f250fb9864a66cacb8
Andrew Hyder <hyd415@gmail.com>
2019-01-31 11:42:37 -0800

    hello world

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source "https://rubygems.org"
+gem "sinatra"
+gem "sinatra-contrib"
+
+gem "mini_magick"
+
+gem "httparty"
+
+gem "pry"
---
+++ app.rb
@@ -0,0 +1,44 @@
+require "sinatra"
+require "sinatra/reloader" if development?
+require "mini_magick"
+require "httparty"
+require "pry"
+
+class App < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  get "/" do
+    erb :form
+  end
+
+  post '/save_image' do
+
+    @filename = params[:file][:filename]
+    file = params[:file][:tempfile]
+
+    File.open("./public/#{@filename}", 'wb') do |f|
+      f.write(file.read)
+    end
+
+    image = MiniMagick::Image.open("./public/#{@filename}")
+    image.resize "1500x750"
+    image.format "jpeg"
+    image.write "./public/output.jpeg"
+
+
+
+    response = HTTParty.post('https://api.ocr.space/parse/image', headers: {apikey: ENV["ocr_apikey"]}, body: { file: File.open("./public/output.jpeg")} )
+    groups = /LN\s*(.+?) \r\nFN\s*(.+?) \r\n(\d+ .+?) \r\n(.+?) \r\n/.match(response["ParsedResults"].first["ParsedText"])
+
+    results = {
+      last_name: groups[1],
+      first_name: groups[2],
+      street_address: groups[3],
+      city_state_zip: groups[4]
+    }
+
+    erb :show_image, locals: { results: results }
+  end
+end
